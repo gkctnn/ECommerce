9c8310a1d6722eefd9420ba192fe94b9cc33450a
gokcenur.zenginal <gokcenur.zenginal@inveon.com.tr>
2020-08-18 20:14:23 +0300

    # Adding Swagger

--- ECommerce.WebApi/Startup.cs
+++ ECommerce.WebApi/Startup.cs
@@ -18,6 +18,10 @@ using ECommerce.Entities.Customer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
+using System;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.IO;
 
 namespace ECommerce.WebApi
 {
@@ -45,25 +49,7 @@ namespace ECommerce.WebApi
                 .AddEntityFrameworkStores<AppDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-                .AddJwtBearer(options =>
-                {
-                    options.SaveToken = true;
-                    options.RequireHttpsMetadata = true;
-                    options.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidateIssuer = true,
-                        ValidIssuer = "https://localhost:44318",
-                        ValidateAudience = true,
-                        ValidAudience = "https://localhost:44318",
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ECommerceApiAuthentication"))
-                    };
-                });
+            
 
             services.AddTransient<ICategoryService, CategoryManager>();
             services.AddTransient<ICategoryRepository, CategoryRepository>();
@@ -71,6 +57,81 @@ namespace ECommerce.WebApi
             services.AddTransient<IProductRepository, ProductRepository>();
             services.AddTransient<IShoppingCartItemService, ShoppingCartItemManager>();
             services.AddTransient<IShoppingCartItemRepository, ShoppingCartItemRepository>();
+
+
+
+            services.AddSwaggerGen((options) =>
+            {
+                options.SwaggerGeneratorOptions.IgnoreObsoleteActions = true;
+                options.SwaggerDoc(Configuration.GetSection("Swagger:SwaggerName").Value, new OpenApiInfo()
+                {
+                    Version = Configuration.GetSection("Swagger:SwaggerDoc:Version").Value,
+                    Title = Configuration.GetSection("Swagger:SwaggerDoc:Title").Value,
+                    Description = Configuration.GetSection("Swagger:SwaggerDoc:Description").Value,
+                    TermsOfService = new Uri(Configuration.GetSection("Swagger:SwaggerDoc:TermsOfService").Value),
+                    Contact = new OpenApiContact
+                    {
+                        Name = Configuration.GetSection("Swagger:SwaggerDoc:Contact:Name").Value,
+                        Email = string.Empty,
+                        Url = new Uri(Configuration.GetSection("Swagger:SwaggerDoc:Contact:Url").Value),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = Configuration.GetSection("Swagger:SwaggerDoc:License:Name").Value,
+                        Url = new Uri(Configuration.GetSection("Swagger:SwaggerDoc:License:Url").Value),
+                    }
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme.",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Scheme = "Bearer",
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "JWT"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+
+                    }
+                });
+            });
+
+            var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "https://localhost:44318",
+                    ValidAudience = "https://localhost:44318",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ECommerceApiAuthentication"))
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -94,6 +155,15 @@ namespace ECommerce.WebApi
             app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(url: String.Format("/swagger/{0}/swagger.json", "JWTUygulamasiWebAPI-Swagger"), name: "Version CoreSwaggerWebAPI-1");
+                //c.DocExpansion(DocExpansion.None);
+
+            });
         }
     }
 }
